3864b0ab25d272947414866a441111baa70a173c
Vincent Davis Jr <vince@underview.tech>
2023-02-05 14:17:50 -0600

    build.sh: add the setup_flash_utility func
    Just clones UDOO Bolt's UEFI Firmware Flash utility
    and adds simple way to flash BIOS Boot ROM.
    
    NOTE UNTESTED AS OF COMMIT
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- build.sh
+++ build.sh
@@ -3,6 +3,29 @@
 CDIR=$(pwd)
 BUILD_THREADS=$(($(nproc)/2))
 
+setup_flash_util() {
+  local bolt_flash_util_dir="${CDIR}/bolt-flash-utility"
+
+  [[ -f "${bolt_flash_util_dir}/Flash_tools/AFU/Linux/64/EtaAfuOemLnx64" ]] || {
+    wget https://www.udoo.org/download/files/UDOO_BOLT/UEFI_update/UDOOBOLT_C40_UEFI_Update_rel108.zip \
+         -O "${CDIR}/uefi-firmware-updateder.zip" || return 1
+
+    mkdir -p "${bolt_flash_util_dir}"
+    unzip "${CDIR}/uefi-firmware-updateder.zip" -d "${bolt_flash_util_dir}" || return 1
+    rm "${CDIR}/uefi-firmware-updateder.zip"
+
+    chmod +x "${bolt_flash_util_dir}/Flash_tools/AFU/Linux/64/bios_updater_x64.sh"
+  }
+
+  [[ -n "${1}" ]] && {
+    ${bolt_flash_util_dir}/Flash_tools/AFU/Linux/64/bios_updater_x64.sh "${1}"
+    cd "${CDIR}"
+  }
+
+  return 0
+}
+
+
 build_coreboot() {
   local coreboot_dir="${CDIR}/coreboot"
 
@@ -12,7 +35,7 @@ build_coreboot() {
     git -C "${coreboot_dir}" submodule update --init --checkout || return 1
     make -C "${coreboot_dir}" crossgcc-i386 CPUS="${BUILD_THREADS}" || return 1
   }
-  
+
   cat < "${CDIR}/amd-zen-coreboot.cfg" >> "${coreboot_dir}/.config"
   make -C "${coreboot_dir}" menuconfig || return 1
   make -C "${coreboot_dir}" -j"${BUILD_THREADS}" || return 1
@@ -20,6 +43,10 @@ build_coreboot() {
   return 0
 }
 
-build_coreboot || exit 1
+[[ "${1}" == "--flash-util" ]] && {
+  setup_flash_util "${2}" || exit 1
+} || {
+  build_coreboot || exit 1
+}
 
 exit 0
